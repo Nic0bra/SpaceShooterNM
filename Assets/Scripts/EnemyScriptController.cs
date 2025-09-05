c46a58cfe846b7aa9804e5ad7b4472ec63862b7e
Nicole <x0obubbleso0x@gmail.com>
2025-09-05 11:55:32 -0500

    other stuff

--- Assets/Scripts/EnemyScriptController.cs
+++ Assets/Scripts/EnemyScriptController.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 public class EnemyScriptController : MonoBehaviour
 {
     public int enemyHealth = 2;
-    public float shipDropSpeed = -3/*Random.Range(2f, 6f)*/;
+    public float shipDropSpeed = -3f;
 
     //Control Sine Movement
     public float _frequency = 6f;
     public float _distance = .75f;
+    public float rotateSpeed = 1000f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,6 +21,7 @@ public class EnemyScriptController : MonoBehaviour
     void Update()
     {
         transform.Translate(0, shipDropSpeed * Time.deltaTime, 0);
+        transform.Rotate(0 , rotateSpeed * Time.deltaTime, 0);
         transform.position = new Vector3(XSine(), 
             transform.position.y,
             transform.position.z);
