31f5d144d3b949605d9735a072692654c5617f0d
Nicole <x0obubbleso0x@gmail.com>
2025-09-05 10:12:37 -0500

    update player ship controller

--- Assets/Scripts/PlayerShipController.cs
+++ Assets/Scripts/PlayerShipController.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class PlayerShipController : MonoBehaviour
 {
+    [SerializeField] public float moveSpeed = 3f;
+
+    //Control ship move distance
+    public float xMin, xMax;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +17,12 @@ public class PlayerShipController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float xMove = Input.GetAxisRaw("Horizontal");
+
+        transform.Translate(xMove * moveSpeed * Time.deltaTime, 0, 0);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, xMin, xMax),
+            transform.position.y,
+            transform.position.z);
+
     }
 }
